5757ef6b6418acb4a58229a72089857768563f8f
David Newbury <david.newbury@gmail.com>
2017-05-01 10:55:31 -0400

    new stuff

--- src/App.js
+++ src/App.js
@@ -10,6 +10,14 @@ class App extends Component {
   constructor(props) {
       super(props);
       this.state = {flipped: false, currentRun: Date.now()};
+      $("body").on('touchmove',function(event){
+        event.preventDefault();
+        return false;
+      });
+      $("body").on('selectstart',function(event){
+        event.preventDefault();
+        return false;
+      });
   }
 
   resetDashboard() {
@@ -28,9 +36,9 @@ class App extends Component {
   }
 
   render() {
-    let chat = `Frans Hals ranks with Rembrandt van Rijn as a great portraitist, distinguished by his dazzling brushwork and penetrating analysis of character. The portrait represents a 73-year-old man, the municipal beadle (court officer) of Leiden and a member of the Leiden society of Rederijkers, or amateur literary club. Its details refer to van der Morsch's role in the club, which composed poetry and staged satirical plays. The dried herring in the sitter's hand refers to the phrase "to give a red herring," which in the Dutch vernacular means "to rebuke someone with sarcastic remarks." The slogan at the upper left of the painting, Wie begeert [Who wants it?], refers to his caustic wit. Van der Morsch himself probably specified the symbolism, which supports the artist's reading of his sitter's character conveyed in the animated pose and expression.`
-    let story = chat
-
+    let chat = `Frans Hals’s portraiture is distinguished by dazzling brushwork and penetrating analysis of character. Hals received numerous commissions for both individual and group portraits, even though his style of visible brushstrokes differed from prevailing norms in Dutch painting at the time. In the 19th century, with the advent of the Impressionist movement, Hals’s mastery was recognized anew.\nHals’s style is well-suited for a portrait of Pieter Cornelisz. van der Morsch, a court officer of Leiden and a member of the Chamber of Rhetoric, the city’s amateur literary club. Details in the painting refer to van der Morsch's role in the club, which composed poetry and staged satirical plays. The dried herring he holds refers to the phrase “to give a red herring,” Dutch slang for rebuking someone with sarcastic remarks. The animated pose and expression convey van der Morsch’s lively character, while the slogan at the upper left, Wie begeert (“Who wants it?”), suggests his wit.`
+    chat = chat.replace("\n","</p></p>")
+    let story = `In the 17th century, Dutch artists often painted on wooden panels even though painting on canvas had become popular elsewhere nearly a century earlier. When the British art dealer Sir Joseph Duveen purchased this Hals portrait from Lord Northbrook in 1927 it was still on its original panel support. By the time he sold it to the American advertising pioneer Alfred W. Erickson in 1929, it had been transferred to canvas. Such transfer, usually to stabilize a painting, is a painstaking process. It involves gradually shaving off the damaged, cracked, or infested panel and attaching the resulting thin paint layer to a specially prepared canvas. The process was used extensively in the 19th century, but it has since been largely supplanted by less severe methods of conservation.`
     let objectData = {
       startYear: 1616
     }
@@ -43,22 +51,22 @@ class App extends Component {
         <section className="front">
           <div className="content">
             <Tombstone />
-            <p>{chat}</p>
+            <p dangerouslySetInnerHTML={{__html: chat}}></p>
           </div>
           <footer>
-            <Button text='Where has this painting been?' cta='Touch here to find out!' onClick={this.spin.bind(this)}/>
+            <Button text='Where has this painting been?' cta='Touch here to find out!' touchEvent={this.spin.bind(this)}/>
           </footer>
         </section>
         <section className="back">
           <div className="content">
             <Tombstone />
             <Dashboard objectData={objectData} key={this.state.currentRun}/>
-            <h2>★ A Moment in History:  1881</h2>
+            <h2>★ A Moment in History:  1927</h2>
             <p>{story}</p>
           </div>
           <footer>
-            <Button text='Reset' cta='Touch Here.' size="half" onClick={resetDashboard}/>
-            <Button text='Go Back' cta='Touch Here.' size="half" onClick={spin}/>
+            <Button text='Replay' cta='Touch Here.' size="half" touchEvent={resetDashboard}/>
+            <Button text='Go Back' cta='Touch Here.' size="half" touchEvent={spin}/>
           </footer>
         </section>
       </div>
--- src/Button.js
+++ src/Button.js
@@ -6,7 +6,7 @@ export default function(props) {
   let button_class = props.size === "half" ? "button half" : "button";
 
   return (
-    <div className={button_class} onClick={props.onClick}>
+    <div className={button_class} onTouchStart={props.touchEvent}>
       <div className="button_text">{props.text}</div>
       <div className="call_to_action">{props.cta}</div>
     </div>
