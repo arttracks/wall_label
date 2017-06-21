c98df78fb645e89cce0df845201bf150d3682e7c
David Newbury <david.newbury@gmail.com>
2017-06-21 09:02:59 -0400

    credits, fixed redirects

--- src/App.js
+++ src/App.js
@@ -14,16 +14,30 @@ import './App.css';
 const TableOfContents = function() {
 
   const ipadTitles = objectData.map(function(work, index){
-    return (<Link to={"/ipad/"+ (index+1)}>{work.title}</Link>)
+    return (<div><Link to={"/ipad/"+ (index+1)}>{work.title}</Link></div>)
   });
 
   return (
       <div className="TOC">
         <h2>iPads</h2>
         {ipadTitles}
-        
+
         <h2>Table</h2>
         <Link to="/table">Table Stories</Link>
+        <div className='credits'>
+          <p>
+            This is part of <a href='http://www.museumprovenance.org' target='_blank'>Art Tracks</a>, a project of <a href='http://cmoa.org' target='_blank'>Carnegie Museum of Art</a>.
+          </p>
+          <p>
+            Source code for this project is available under a <a href='https://opensource.org/licenses/MIT' target='_blank'>MIT license</a> on <a href='https://github.com/arttracks/wall_label' target='_blank'> Github</a> for download, modification, or reuse.  All written content is released under a <a href='https://creativecommons.org/publicdomain/zero/1.0/' target='_blank'> Creative Commons 1.0 Universal</a> license for reuse.
+          </p>
+          <p>
+            Thank you to <a href='https://www.netlify.com' target='_blank'>Netlify</a> for providing free hosting to this project and other open-source software.
+          </p>
+          <p>
+            <em>Initial funding for <strong>Art Tracks</strong> was provided in part by a generous grant by the <a href="http://www.imls.gov">Institute of Museum and Library Services</a>.  Funding for Phase II has been provided by the <a href="http://www.neh.gov/">National Endowment for the Humanities</a> with additional research support provided by the <a href="http://www.kressfoundation.org">Samuel H. Kress Foundation</a> and the <a href="http://www.paul-mellon-centre.ac.uk/">Paul Mellon Centre for Studies in British Art</a>.</em>
+          </p>
+        </div>
       </div>
   )
 }
