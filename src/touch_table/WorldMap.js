41b88683f60655f1d6e0d6460933201441a5f33b
David Newbury <david.newbury@gmail.com>
2017-09-17 20:50:10 -0700

    added changes

--- src/touch_table/WorldMap.js
+++ src/touch_table/WorldMap.js
@@ -36,9 +36,62 @@ class WorldMap extends Component {
         .enter().append( 'path' )
         .attr( 'class', 'land' )
         .attr( 'd', path );
+    this.setState({svg: svg, projection: projection})
+
+
+    var radialGradient = svg.append("defs")
+      .append("radialGradient")
+        .attr("id", "radial-gradient");
+
+    radialGradient.append("stop")
+        .attr("offset", "%")
+        .attr("stop-color", "rgba(0,0,0,1)");
+    radialGradient.append("stop")
+        .attr("offset", "20%")
+        .attr("stop-color", "rgba(0,0,0,0.05)");
+    radialGradient.append("stop")
+        .attr("offset", "100%")
+        .attr("stop-color", "rgba(0,0,0,0)");
   }
 
+  processData(data, currentYear) {
+    return data.map( e=> {
+      const firstValid = e.events.findIndex(event => event.year <= currentYear)
+      if (firstValid === -1) {return null}
+      
+      let lastValid = e.events.findIndex(event => event.year > currentYear) -1
+      if (lastValid === -2) {lastValid = (e.events.length -1)}    
+
+      if (e.events[lastValid] === undefined) { console.log("problem",lastValid,e); return null}
+      return {id: e.id, lat: e.events[lastValid].lat, lng: e.events[lastValid].lng}
+    }).filter(n => n)
+  }
+
+
   componentWillReceiveProps(nextProps) {
+
+    const processedData = this.processData(eventData, nextProps.currentYear);
+    const projection = this.getProjection();
+    let circles = this.state.svg.selectAll("g.point").data(processedData, function(d) { return d.id; })
+
+    circles.exit().remove();
+
+    let groups = circles.enter().append("g").attr("class", "point")
+      // groups.append("circle").attr("class", "dot")
+      groups.append("circle").attr("class", "heatmap").style("fill", "url(#radial-gradient)");
+
+
+    circles = groups.merge(circles);
+    circles
+      .attr("transform",d=> {const loc = projection([d.lng,d.lat]); return `translate(${loc[0]},${loc[1]})`})
+    // circles.selectAll(".dot")
+    //   .attr("r", 2)
+    circles.selectAll(".heatmap")
+      .attr("r", 5)
+        
+
+
+   // debugger
     // Do something here with dots.
   }
 
@@ -54,8 +107,8 @@ class WorldMap extends Component {
     const offset = [width / 2, height / 2 ];
     const projection = d3.geoEquirectangular().scale( scale ).rotate( [0,0] ).center([0,5]).translate( offset );
    
-    projection.scale( 800 );
-    projection.center([7.8578, 52.9762])
+    projection.scale( 1600 );
+    projection.center([8.8578, 46.9762])
 
     return projection;
   }
